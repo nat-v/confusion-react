3df15cda4b00bc9a19f9d65b7655021e783ad48b
Nataliia Volynets <natasha99zt@gmail.com>
2020-05-12 19:02:21 +0300

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     );
   }
 }
